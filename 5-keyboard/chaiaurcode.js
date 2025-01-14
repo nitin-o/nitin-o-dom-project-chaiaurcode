7ecf36b0c61a03a7fae19c35cf08b9e4078e7101
nitin-o <nitinotavkar@gmail.com>
2025-01-14 15:36:39 +0530

    05_key_board

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,20 @@
-console.log('Project 5');
+const insert = document.querySelector("#insert");
+ window.addEventListener("keydown",(e)=>{
+    insert.innerHTML=`
+    <div>
+    <table>
+        <tr>
+            <td>key</td>
+            <td>key code</td>
+            <td>coed</td>
+        </tr>
+        <tr>
+            <td>${e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code}</td>
+        </tr>
+       </table>
+    </div>
+    
+    `
+ })
